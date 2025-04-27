a1909a14f69a8e6495e842ffaf531a55368c0833
Rakhi Kumari <rakhikumari@Rakhi-MacBook-Air.local>
2025-04-27 15:21:13 +0530

    minor bugs fixed

--- VideoDownloaderPlayer/App/Model/Video.swift
+++ VideoDownloaderPlayer/App/Model/Video.swift
@@ -23,19 +23,33 @@ class Video {
     
 }
 
-struct DownloadedVideo: Codable{
+struct DownloadedVideo: Codable {
     let remoteUrl: URL?
     let downloadedUrl: URL?
 }
 
-extension UserDefaults{
+extension UserDefaults {
     var downloadedVideos: [DownloadedVideo]? {
-        get{
-            try? JSONDecoder().decode([DownloadedVideo].self, from: data(forKey: "downloadedVideos") ?? Data())
+        get {
+            guard let data = data(forKey: "downloadedVideos"),
+                  var videos = try? JSONDecoder().decode([DownloadedVideo].self, from: data)
+            else { return nil }
+            
+            let documentsDirectory = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first
+            
+            // Update each downloadedUrl dynamically
+            videos = videos.map { video in
+                guard let lastPath = video.downloadedUrl?.lastPathComponent else {
+                    return video
+                }
+                let updatedUrl = documentsDirectory?.appendingPathComponent(lastPath)
+                return DownloadedVideo(remoteUrl: video.remoteUrl, downloadedUrl: updatedUrl)
+            }
+            
+            return videos
         }
-        set{
+        set {
             UserDefaults.standard.set(try? JSONEncoder().encode(newValue ?? []), forKey: "downloadedVideos")
         }
     }
-    
 }
